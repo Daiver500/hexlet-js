6ff08428feb28a00ee0d2599b7b3ebea6bc1aa27
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2021-02-23 14:18:52 +0300

    test

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log ("Привет хекслет")
+
+// Так будет происходить поиск файла lodash.js в текущей директории
+import _ from "./lodash";
+
+// Так импортируется код из пакета
+import _ from "lodash";
+
+console.log(_.last(['one', 'two']));
